8d49b88bf234241041ee74e362a6d8b36f2fb3ce
Narcisse <mechikaegon@gmail.com>
2024-08-22 12:35:53 +0100

    Update web search method

--- src/search/search.ts
+++ src/search/search.ts
@@ -3,10 +3,10 @@ import { SearchParams, SearchResponse, SuggestionResponse } from "./interfaces";
 class Search {
   constructor(private readonly client: RequestClient) {}
   search = async (params: SearchParams): Promise<SearchResponse> => {
-    return await this.client.fetchJSS("/ai/scrape", "POST", params);
+    return await this.client.fetchJSS("/web/search", "GET", undefined, params);
   };
   suggestion = async (query: string): Promise<SuggestionResponse> => {
-    return await this.client.fetchJSS(`/web/search/suggest?query=${query}`, "GET", {});
+    return await this.client.fetchJSS(`/web/search/suggest?query=${query}`, "GET", undefined);
   };
 }
 
--- src/web/interfaces/html_to_any.ts
+++ src/web/interfaces/html_to_any.ts
@@ -1,20 +1,20 @@
 export interface HTMLAnyParams {
-  html: string;
-  url: string;
+  html?: string;
+  url?: string;
   use_graphic_renderer?: boolean;
   dark_mode?: boolean;
-  is_mobile: boolean;
-  pdf_page_range: string;
-  pdf_print_background: boolean;
-  pdf_display_header_footer: boolean;
-  size_preset: string;
-  height: number;
-  width: number;
-  type: string;
-  quality: number;
-  omit_background: boolean;
-  full_page: boolean;
-  scale: number;
+  is_mobile?: boolean;
+  pdf_page_range?: string;
+  pdf_print_background?: boolean;
+  pdf_display_header_footer?: boolean;
+  size_preset?: string;
+  height?: number;
+  width?: number;
+  type?: string;
+  quality?: number;
+  omit_background?: boolean;
+  full_page?: boolean;
+  scale?: number;
   goto_options?: {
     timeout: number;
     wait_until: string;
