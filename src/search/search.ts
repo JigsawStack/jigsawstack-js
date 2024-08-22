5afd97663a6305e2b2729efa43fffb8620f5ee82
Yoeven D Khemlani <yoevenkhem@gmail.com>
2024-08-21 23:32:03 -0400

    Updated search mapping

--- src/search/search.ts
+++ src/search/search.ts
@@ -2,7 +2,7 @@ import { RequestClient } from "../request";
 import { SearchParams, SearchResponse, SuggestionResponse } from "./interfaces";
 class Search {
   constructor(private readonly client: RequestClient) {}
-  ai = async (params: SearchParams): Promise<SearchResponse> => {
+  search = async (params: SearchParams): Promise<SearchResponse> => {
     return await this.client.fetchJSS("/ai/scrape", "POST", params);
   };
   suggestion = async (query: string): Promise<SuggestionResponse> => {
