ff51742820e06ca5daa4cd1efe82b14996032b6d
Narcisse <mechikaegon@gmail.com>
2024-09-03 15:11:02 +0100

    clean up

--- src/store/file.ts
+++ src/store/file.ts
@@ -5,10 +5,18 @@ import { FileUploadParams, FileUploadResponse } from "./interfaces/file";
 export class File {
   constructor(private readonly client: RequestClient) {}
   upload = async (file: Blob | Buffer, params: FileUploadParams): Promise<FileUploadResponse> => {
-    return await this.client.fetchJSS(`/store/file`, "POST", file, {
-      key: params.filename,
-      overwrite: params.overwrite,
-    });
+    return await this.client.fetchJSS(
+      `/store/file`,
+      "POST",
+      file,
+      {
+        key: params.filename,
+        overwrite: params.overwrite,
+      },
+      params?.content_type && {
+        "Content-Type": params.content_type,
+      }
+    );
   };
   retrieve = async (key: string) => {
     const resp = await this.client.fetchJSS(`/store/file/${key}`, "GET", {});
--- src/store/interfaces/file.ts
+++ src/store/interfaces/file.ts
@@ -3,6 +3,7 @@ import { BaseResponse } from "../../../types";
 export interface FileUploadParams {
   overwrite?: boolean;
   filename: string;
+  content_type?: string;
 }
 
 export interface FileRetrieveParams {
