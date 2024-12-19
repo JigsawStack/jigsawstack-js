e45ecb48401fc87cbfe8859a63e63035bd9d11d7
Narcisse <mechikaegon@gmail.com>
2024-12-19 16:56:46 +0100

    clean up

--- src/search/search.ts
+++ src/search/search.ts
@@ -3,7 +3,7 @@ import { SearchParams, SearchResponse, SuggestionResponse } from "./interfaces";
 class Search {
   constructor(private readonly client: RequestClient) {}
   search = async (params: SearchParams): Promise<SearchResponse> => {
-    return await this.client.fetchJSS("/web/search", "GET", undefined, params);
+    return await this.client.fetchJSS("/web/search", "POST", params);
   };
   suggestion = async (query: string): Promise<SuggestionResponse> => {
     return await this.client.fetchJSS(`/web/search/suggest?query=${query}`, "GET", undefined);
