9aa482959d26697fefcfb0ac14ecd4925a74f9ed
Khurdhula-Harshavardhan <khurdhulaharshavardhan@gmail.com>
2026-07-06 14:00:31 -0700

    feat(obj-det): allow users to enhance prompts

--- src/vision/interfaces.ts
+++ src/vision/interfaces.ts
@@ -38,6 +38,7 @@ export type ObjectDetectionParams = {
   url?: string;
   file_store_key?: string;
   prompts?: string[];
+  enhance_prompts?: boolean;
   features?: ("object" | "gui")[];
   annotated_image?: boolean;
   return_type?: "url" | "base64";
